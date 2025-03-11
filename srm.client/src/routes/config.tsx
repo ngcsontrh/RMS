edfd5e98257bcb64c842576af899e04d654818bb
danhquyen2004 <mynamepow@gmail.com>
2025-03-12 02:03:37 +0700

    add some page: hdsd, quyche, lienhe

--- srm.client/src/routes/config.tsx
+++ srm.client/src/routes/config.tsx
@@ -1,6 +1,9 @@
 import { Routes, Route } from 'react-router-dom';
 import { DeTaiHome, DeTaiEdit } from '../components/DeTai';
 import { DanhMucHome } from '../components/DanhMuc';
+import LienHe from "../components/LienHe/LienHe"
+import QuyChe from "../components/QuyChe/QuyChe";
+import HuongDanSuDung from '../components/HuongDanSuDung/HuongDanSuDung';
 
 const AppRoutes: React.FC = () => {
     return (
@@ -12,6 +15,9 @@ const AppRoutes: React.FC = () => {
             <Route path="/de-tai/tao-moi" element={ <DeTaiEdit />} />
             <Route path="/de-tai/:id" element={<DeTaiEdit />} />
             <Route path="/danh-muc" element={<DanhMucHome />} />
+            <Route path="/lien-he" element={<LienHe />} />
+            <Route path="/quy-che" element={<QuyChe />} />
+            <Route path="/huong-dan-su-dung" element={<HuongDanSuDung />} />
         </Routes>
     )
 }
