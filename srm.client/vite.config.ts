f225da3357ff8c7ae1d9aa88c7c0264890897d6b
sontn <sontn@softdreams.vn>
2025-03-14 16:55:15 +0700

    up

--- srm.client/vite.config.ts
+++ srm.client/vite.config.ts
@@ -7,31 +7,31 @@ import path from 'path';
 import child_process from 'child_process';
 import { env } from 'process';
 
-// const baseFolder =
-//     env.APPDATA !== undefined && env.APPDATA !== ''
-//         ? `${env.APPDATA}/ASP.NET/https`
-//         : `${env.HOME}/.aspnet/https`;
+const baseFolder =
+    env.APPDATA !== undefined && env.APPDATA !== ''
+        ? `${env.APPDATA}/ASP.NET/https`
+        : `${env.HOME}/.aspnet/https`;
 
-// const certificateName = "srm.client";
-// const certFilePath = path.join(baseFolder, `${certificateName}.pem`);
-// const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
+const certificateName = "srm.client";
+const certFilePath = path.join(baseFolder, `${certificateName}.pem`);
+const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
 
-// if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
-//     if (0 !== child_process.spawnSync('dotnet', [
-//         'dev-certs',
-//         'https',
-//         '--export-path',
-//         certFilePath,
-//         '--format',
-//         'Pem',
-//         '--no-password',
-//     ], { stdio: 'inherit', }).status) {
-//         throw new Error("Could not create certificate.");
-//     }
-// }
+if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
+    if (0 !== child_process.spawnSync('dotnet', [
+        'dev-certs',
+        'https',
+        '--export-path',
+        certFilePath,
+        '--format',
+        'Pem',
+        '--no-password',
+    ], { stdio: 'inherit', }).status) {
+        throw new Error("Could not create certificate.");
+    }
+}
 
 const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-    env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:44387';
+    env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:7106';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -49,9 +49,9 @@ export default defineConfig({
             }
         },
         port: 5173,
-        // https: {
-        //     key: fs.readFileSync(keyFilePath),
-        //     cert: fs.readFileSync(certFilePath),
-        // }
+        https: {
+            key: fs.readFileSync(keyFilePath),
+            cert: fs.readFileSync(certFilePath),
+        }
     }
 })
